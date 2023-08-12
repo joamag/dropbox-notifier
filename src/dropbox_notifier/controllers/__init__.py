5657caf0597dafa5a6948d11e04d6c594f458365
João Magalhães <joamag@gmail.com>
2023-08-13 00:10:48 +0100

    chore: removed unused webhook routes

--- src/dropbox_notifier/controllers/__init__.py
+++ src/dropbox_notifier/controllers/__init__.py
@@ -5,10 +5,8 @@ from . import admin
 from . import base
 from . import oauth
 from . import root
-from . import webhook
 
 from .admin import AdminController
 from .base import BaseController
 from .oauth import OauthController
 from .root import RootController
-from .webhook import WebhookController
