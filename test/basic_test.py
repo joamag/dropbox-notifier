565bcf7636a4d1906d7bdc56953972a1bb0fb427
João Magalhães <joamag@gmail.com>
2024-03-27 10:25:39 +0000

    fix: new python versions

--- test/basic_test.py
+++ test/basic_test.py
@@ -1,6 +1,12 @@
 from unittest import IsolatedAsyncioTestCase
 
+from dropbox_notifier import DropboxNotifierApp
+
 
 class BasicCache(IsolatedAsyncioTestCase):
     def test_simple(self):
         self.assertEqual(2 + 2, 4)
+
+    def test_instantiate(self):
+        app = DropboxNotifierApp()
+        self.assertNotEqual(app, None)
